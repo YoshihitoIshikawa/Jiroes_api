83f154ac5f908b88a0498e9a336873c2e6fad4a5
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-01 16:46:35 +0900

    :add:user, reviewモデルにlikeモデルの紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   self.primary_key = :sub
 
   has_many :reviews, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def self.from_token_payload(payload)
     find_by(sub: payload['sub']) || create!(sub: payload['sub'])
