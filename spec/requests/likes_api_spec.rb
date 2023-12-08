6aafa63d2cd5d82f4b4ae4d4b4e17077b1e6c7f8
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-08 12:55:00 +0900

    :add:likesのリクエストスペック実装
    index, create, destroyのテスト実装

---
+++ spec/requests/likes_api_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::LikesController", type: :request do
+  let!(:current_user) { create(:user) }
+  let!(:shop) { create(:shop) }
+  let!(:review) { create(:review, shop: shop, sub: current_user.sub) }
+  let!(:like) { create(:like, shop: shop, review: review, sub: current_user.sub) }
+
+  before do
+    authorization_stub
+  end
+
+  describe "GET /api/v1/shops/:shop_id/reviews/:review_id/likes" do
+    it "returns all likes for a review" do
+      get api_v1_shop_review_likes_path(shop.id, review.id)
+
+      expect(response.status).to eq(200)
+
+      reviews_json = JSON.parse(response.body)
+      expect(reviews_json.length).to eq(1)
+    end
+  end
+
+  describe "POST /api/v1/shops/:shop_id/reviews/:review_id/likes" do
+    it "returns all likes for a review" do
+      like_params = {
+        sub: current_user.sub,
+        shop_id: shop.id,
+        review_id: review.id,
+      }
+
+      post api_v1_shop_review_likes_path(shop.id, review.id), params: like_params
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'DELETE /api/v1/shops/:shop_id/reviews/:review_id/likes/:like_id' do
+    it 'deletes a review' do
+      delete api_v1_shop_review_like_path(shop.id, review.id, like.id)
+
+      expect(response.status).to eq(200)
+    end
+  end
+end
