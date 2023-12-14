ad11c627e61499f31d45e627042ec4a977333e48
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-14 19:38:57 +0900

    :update:liked_review_controller実装
    いいねした投稿のみを取得するindexアクションを実装

--- app/controllers/api/v1/liked_reviews_controller.rb
+++ app/controllers/api/v1/liked_reviews_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::LikedReviewsController < SecuredController
-  # skip_before_action :authorize_request
-
   def index
-    liked_reviews = @current_user.likes.map(&:review)
+    likes = Like.where(sub: @current_user.sub)
+    review_ids = likes.map { |like| like['review_id'] }
+    liked_reviews = Review.where(id: review_ids)
     render json: liked_reviews
   end
 end
