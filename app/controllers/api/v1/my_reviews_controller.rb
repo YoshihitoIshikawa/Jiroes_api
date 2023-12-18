db30139aa0a68ae18ed274ff1ff9700c118ce9ea
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-18 16:56:57 +0900

    :fix:モデル名の記述に誤りがあった為修正
    Reviews→Review

--- app/controllers/api/v1/my_reviews_controller.rb
+++ app/controllers/api/v1/my_reviews_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MyReviewsController < SecuredController
   def index
-    reviews = Reviews.where(sub: @current_user.sub).order(created_at: :desc)
+    reviews = Review.where(sub: @current_user.sub).order(created_at: :desc)
     render json: reviews
   end
 end
