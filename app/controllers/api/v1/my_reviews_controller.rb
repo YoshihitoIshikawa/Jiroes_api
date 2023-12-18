62ef9a912819365daa27c25989674fd80e4fef77
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-18 18:05:52 +0900

    :update:フロントに返すjsonデータに各レビューに紐づいた店舗名を含めるよう修正
    フロントで表示する際にどの店舗のレビューなのかわかるようにする為

--- app/controllers/api/v1/my_reviews_controller.rb
+++ app/controllers/api/v1/my_reviews_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MyReviewsController < SecuredController
   def index
-    reviews = Review.where(sub: @current_user.sub).order(created_at: :desc)
-    render json: reviews
+    reviews = Review.includes(:shop).where(sub: @current_user.sub).order(created_at: :desc)
+    render json: reviews, include: { shop: { only: :name } }
   end
 end
