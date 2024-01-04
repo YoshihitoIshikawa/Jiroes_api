0d3d9d83d95be1ae93940e038a786580825ec674
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2024-01-04 17:06:45 +0900

    :update:liked_reviewコントローラーの戻り値にreviewに紐づくshopのデータを付与
    shopのnameカラムを付与しフロント側で店舗名を表示する事でユーザーにどの店舗のレビューなのか伝えるため

--- app/controllers/api/v1/liked_reviews_controller.rb
+++ app/controllers/api/v1/liked_reviews_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LikedReviewsController < SecuredController
   def index
     likes = Like.where(sub: @current_user.sub).order(created_at: :desc)
     review_ids = likes.pluck(:review_id)
-    liked_reviews = Review.where(id: review_ids).index_by(&:id).values_at(*review_ids)
-    render json: liked_reviews
+    liked_reviews = Review.where(id: review_ids).includes(:shop).index_by(&:id).values_at(*review_ids)
+    render json: liked_reviews, include: { shop: { only: :name } }
   end
 end
